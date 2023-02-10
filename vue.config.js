da81a409bd205028ca053a0328a20279b31a4612
zhangyao2 <zhangyao2@sunline.cn>
2023-02-10 09:49:36 +0800

    mock服务接入配置

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,14 @@ module.exports = defineConfig({
   productionSourceMap: false,
   devServer: {
     proxy: {
+      '/api/mock': {
+        target: process.env.VUE_APP_BASE_URL_MOCK,
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/mock': ''
+        }
+      },
       '/api': {
         target: process.env.VUE_APP_BASE_URL,
         ws: true,
